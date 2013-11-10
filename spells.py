98ee52b8260c26469831413215bf8c932cde34de
Strange-Looper <edelman.jd@gmail.com>
2013-11-10 16:26:45 -0500

    Update spells.py

--- spells.py
+++ spells.py
@@ -18,7 +18,7 @@ class Heal: #heals the player if they are within a radius of 100, otherwise does
                 self.effects=newEffects
                 self.end = 0
         def run(self):
-                if (self.effects.game.player.x-self.effects.x)**2+(self.effects.game.player.y-self.effects.y)**2 <= 100:
+                if (self.effects.getX()-self.effects.x)**2+(self.effects.getY()-self.effects.y)**2 <= 100:
                         self.end = 0
                         self.effects.healWithinRadius(50,100)
                 else:
